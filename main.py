30f199461b02871ff97e46fc5a380c8971b3b2c6
TheVitorGomez <7vitorgomez@gmail.com>
2023-04-22 11:13:44 -0300

    New obstacle (fly)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+from random import randint
 
 
 def display_score():
@@ -9,6 +10,31 @@ def display_score():
     screen.blit(score, score_rect)
 
     return current_score
+
+
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle in obstacle_list:
+            obstacle.x -= 5
+            if obstacle.bottom == 300:
+                screen.blit(snail, obstacle)
+            else:
+                screen.blit(fly, obstacle)
+
+        obstacle_list = [
+            obstacle for obstacle in obstacle_list if obstacle.x > -100]
+
+        return obstacle_list
+
+    return []
+
+
+def collisions(player, obstacles):
+    if obstacles:
+        for obstacle in obstacles:
+            if player.colliderect(obstacle):
+                return False
+    return True
 
 
 WIDTH = 800
@@ -32,14 +58,18 @@ game_name_rect = game_name.get_rect(center=(WIDTH / 2, 50))
 restart = font.render('Press space to run', False, (111, 196, 169))
 restart_rect = restart.get_rect(center=(WIDTH / 2, 350))
 
+# Obstacles
 snail = pygame.image.load('assets/snail/snail1.png').convert_alpha()
-snail_rect = snail.get_rect(bottomright=(800, HEIGHT - 100))
+fly = pygame.image.load('assets/Fly/Fly1.png').convert_alpha()
+
+obstacle_rect_list = []
 
 player = pygame.image.load('assets/Player/player_walk_1.png').convert_alpha()
 player_rect = player.get_rect(midbottom=(80, 300))
-
 player_name = font.render('Caio', True, 'Black')
 player_name_rect = player_name.get_rect(midbottom=(player_rect.midtop))
+player_gravity = 0
+turn_around = True
 
 player_stand = pygame.image.load(
     'assets/Player/player_stand.png').convert_alpha()
@@ -48,8 +78,8 @@ player_stand = pygame.transform.scale(
 player_stand_rect = player_stand.get_rect(
     center=(WIDTH / 2, HEIGHT / 2))
 
-player_gravity = 0
-turn_around = True
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1500)
 
 while True:
     for event in pygame.event.get():
@@ -67,13 +97,20 @@ while True:
                 if event.key == pygame.K_d and turn_around == False:
                     player = pygame.transform.flip(player, True, False)
                     turn_around = True
+            if event.type == obstacle_timer:
+                if randint(0, 2):
+                    obstacle_rect_list.append(
+                        fly.get_rect(bottomright=(randint(900, 1100), 210)))
+                else:
+                    obstacle_rect_list.append(
+                        snail.get_rect(bottomright=(randint(900, 1100), 300)))
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 playing = True
-                snail_rect.right = 800
-                player_rect.left = 80
-                player_name_rect.left = 80
                 start_time = int(pygame.time.get_ticks() / 1000)
+                if turn_around == False:
+                    player = pygame.transform.flip(player, True, False)
+                    turn_around = True
 
     if playing:
         # Background
@@ -82,12 +119,6 @@ while True:
 
         # Score
         score = display_score()
-
-        # Snail
-        snail_rect.left -= 2
-        if snail_rect.right < 0:
-            snail_rect.left = 800
-        screen.blit(snail, snail_rect)
 
         # Player
         player_gravity += 1
@@ -110,10 +141,16 @@ while True:
             player_rect.x += 3
             player_name_rect.x += 3
 
-        # Collision
-        if snail_rect.colliderect(player_rect):
-            playing = False
+        # Obstacle movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
+        # Collisions
+        playing = collisions(player_rect, obstacle_rect_list)
     else:
+        obstacle_rect_list.clear()
+        player_rect.midbottom = (80, 300)
+        player_name_rect.midbottom = (80, 290)
+        player_gravity = 0
         screen.fill((94, 129, 162))
         screen.blit(player_stand, player_stand_rect)
         total_score = font.render(
